d5d6963d4041170a70decdc7831d6b889a121265
biu <biub7596@gmail.com>
2025-08-30 17:51:21 +0700

    Initial Commit

---
+++ MedsDispenser/MedsDispenserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedsDispenserApp.swift
+//  MedsDispenser
+//
+//  Created by dangnguyen on 30/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MedsDispenserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
